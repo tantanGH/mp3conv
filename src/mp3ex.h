af0f73f68bd0fc01b07b4ad85222751e6fc92c3b
tantanGH <tantangh68@gmail.com>
2023-02-23 21:18:01 +0900

    memory bug fix

--- src/mp3ex.h
+++ src/mp3ex.h
@@ -1,12 +1,13 @@
 #ifndef __H_MP3EX__
 #define __H_MP3EX__
 
-#define VERSION "0.3.5 (2023/02/23)"
+#define VERSION "0.3.6 (2023/02/23)"
 
 #define MAX_PATH_LEN (256)
 
 #define MP3_STAGING_BUFFER_BYTES (65536*2)
 
+#define OUTPUT_FORMAT_NONE      (-1)
 #define OUTPUT_FORMAT_ADPCM     (0)
 #define OUTPUT_FORMAT_PCM       (1)
 #define OUTPUT_FORMAT_NAS_ADPCM (2)
